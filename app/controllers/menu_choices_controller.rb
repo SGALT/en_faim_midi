5b5170926d5b1b6c90021b80166c1f2bb1a9a01d
Sylvain Galtier <sylvaingaltier@gmail.com>
2021-02-01 01:28:28 +0100

    job done

--- app/controllers/menu_choices_controller.rb
+++ app/controllers/menu_choices_controller.rb
@@ -1,7 +1,126 @@
 class MenuChoicesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     @menus = Menu.where(date: Date.today..DateTime::Infinity.new)
     @clients = Client.all
+  end
+
+  def edit
+    @menu = Menu.find(params[:id])
+  end
+
+  def change
+    @menu = Menu.find(params[:id])
+    @menu_alt = Menu.where(date: @menu.date).where.not(name: @menu.name)
+    @params_array = (params[:menu][:client_ids] - [""]).map { |str| str.to_i }
+    @previous_array = @menu.clients.ids
+    if @previous_array.count > @params_array.count
+      results = @previous_array - @params_array
+      results.each do |result|
+        client = Client.find(result)
+        @menu.menu_choices.find_by(client_id: result).destroy
+        case client.formule
+        when "7C"
+          @menu.menu_items.each do |menu_item|
+            quantity = menu_item.quantity -= 1
+            menu_item.update(quantity: quantity)
+          end
+        when "2C"
+          @menu.menu_items.each do |menu_item|
+            if menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT"
+              quantity = menu_item.quantity -= 1
+              menu_item.update(quantity: quantity)
+            end
+          end
+        when "3C"
+          if menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT" || menu_item.category == "PAIN"
+            quantity = menu_item.quantity -= 1
+            menu_item.update(quantity: quantity)
+          end
+        when "10C entrée"
+          @menu.menu_items.each do |menu_item|
+            minus_items(menu_item)
+          end
+          @menu_alt.first.menu_items.each do |menu_item|
+            if menu_item.category == "ENTREE" || menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT"
+              minus_items(menu_item)
+            end
+          end
+        when "10C potage"
+          @menu.menu_items.each do |menu_item|
+            minus_items(menu_item)
+          end
+          @menu_alt.first.menu_items.each do |menu_item|
+            if menu_item.category == "POTAGE" || menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT"
+              minus_items(menu_item)
+            end
+          end
+        when "10C dessert"
+          @menu.menu_items.each do |menu_item|
+            minus_items(menu_item)
+          end
+          @menu_alt.first.menu_items.each do |menu_item|
+            if menu_item.category == "DESSERT" || menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT"
+              minus_items(menu_item)
+            end
+          end
+        end
+      end
+    elsif @params_array.count > @previous_array.count
+      results = @params_array - @previous_array
+      results.each do |result|
+        client = Client.find(result)
+        MenuChoice.create(menu: @menu, client: client)
+        case client.formule
+        when "7C"
+          @menu.menu_items.each do |menu_item|
+            quantity = menu_item.quantity += 1
+            menu_item.update(quantity: quantity)
+          end
+        when "2C"
+          @menu.menu_items.each do |menu_item|
+            if menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT"
+              quantity = menu_item.quantity += 1
+              menu_item.update(quantity: quantity)
+            end
+          end
+        when "3C"
+          if menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT" || menu_item.category == "PAIN"
+            quantity = menu_item.quantity += 1
+            menu_item.update(quantity: quantity)
+          end
+        when "10C entrée"
+          @menu.menu_items.each do |menu_item|
+            update_items(menu_item)
+          end
+          @menu_alt.first.menu_items.each do |menu_item|
+            if menu_item.category == "ENTREE" || menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT"
+              update_items(menu_item)
+            end
+          end
+        when "10C potage"
+          @menu.menu_items.each do |menu_item|
+            update_items(menu_item)
+          end
+          @menu_alt.first.menu_items.each do |menu_item|
+            if menu_item.category == "POTAGE" || menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT"
+              update_items(menu_item)
+            end
+          end
+        when "10C dessert"
+          @menu.menu_items.each do |menu_item|
+            update_items(menu_item)
+          end
+          @menu_alt.first.menu_items.each do |menu_item|
+            if menu_item.category == "DESSERT" || menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT"
+              update_items(menu_item)
+            end
+          end
+        end
+      end
+    end
+    redirect_to menu_choices_path
   end
 
   def new
@@ -24,13 +143,13 @@ class MenuChoicesController < ApplicationController
           end
         when "2C"
           @menu.menu_items.each do |menu_item|
-            if menu_item.category == "plat" || menu_item.category == "accompagnement"
+            if menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT"
               quantity = menu_item.quantity += 1
               menu_item.update(quantity: quantity)
             end
           end
         when "3C"
-          if menu_item.category == "plat" || menu_item.category == "accompagnement" || menu_item.category == "pain"
+          if menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT" || menu_item.category == "PAIN"
             quantity = menu_item.quantity += 1
             menu_item.update(quantity: quantity)
           end
@@ -39,7 +158,7 @@ class MenuChoicesController < ApplicationController
             update_items(menu_item)
           end
           @menu_alt.first.menu_items.each do |menu_item|
-            if menu_item.category == "entrée" || menu_item.category == "plat" || menu_item.category == "accompagnement"
+            if menu_item.category == "ENTREE" || menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT"
               update_items(menu_item)
             end
           end
@@ -48,7 +167,7 @@ class MenuChoicesController < ApplicationController
             update_items(menu_item)
           end
           @menu_alt.first.menu_items.each do |menu_item|
-            if menu_item.category == "potage" || menu_item.category == "plat" || menu_item.category == "accompagnement"
+            if menu_item.category == "POTAGE" || menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT"
               update_items(menu_item)
             end
           end
@@ -57,7 +176,7 @@ class MenuChoicesController < ApplicationController
             update_items(menu_item)
           end
           @menu_alt.first.menu_items.each do |menu_item|
-            if menu_item.category == "dessert" || menu_item.category == "plat" || menu_item.category == "accompagnement"
+            if menu_item.category == "DESSERT" || menu_item.category == "PLAT" || menu_item.category == "ACCOMPAGNEMENT"
               update_items(menu_item)
             end
           end
@@ -65,8 +184,6 @@ class MenuChoicesController < ApplicationController
       end
     end
     redirect_to menu_choices_path
-    # @menu_choice = MenuChoice.new(menu_choice_params)
-    # raise
   end
 
   private
@@ -77,6 +194,11 @@ class MenuChoicesController < ApplicationController
 
   def update_items(menu_item)
     quantity = menu_item.quantity += 1
+    menu_item.update(quantity: quantity)
+  end
+
+  def minus_items(menu_item)
+    quantity = menu_item.quantity -= 1
     menu_item.update(quantity: quantity)
   end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,7 @@
 class Client < ApplicationRecord
   belongs_to :user
   has_many :menu_choices, dependent: :nullify
+  has_many :menus, through: :menu_choices
   validates :name, presence: true
   validates :formule, presence: true
   validates :specificity, presence: true
